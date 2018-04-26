d2a1f15e77f99d7f973017c84609a95b66e5f80e
Anthony Mockler <anthony@iamcarbonatedmilk.com>
2018-04-26 18:14:57 +0800

    Created a decent interface, no API connections yet

--- app.py
+++ app.py
@@ -11,17 +11,55 @@ app.debug = True
 app.secret_key = "q9283jrisadjfklasdfoqiwe82934u329sdf"
 
 
+def create_authzero_application(authzero_token):
+    #Create a new client application
+        #Params are Callback, Logout, CORS, Web Origins, OIDC Off
+    #Save Domain Name, Client ID, Client Secret from response
+    #Create Auth0 rule
+    return()
+
+def create_twilio_application(twilio_account_sid,twilio_auth_token):
+    #Collect the User's account Sid, Message Service Sid, Auth Token
+    #Create a Twilio Messaging Service https://www.twilio.com/docs/sms/services/api#create-a-service
+        # Params: Process Inbound Messages, Inbound Request URL, Status Callback URL
+    # Collect account_sid, sid
+    # From somewhere else? collect Twilio auth token
+    # Purchase a Twilio number and tell the user what it is
+    return()
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     session['steps'] = 99
-    # What do you want to call your app?
-    # Validation rules: lowercase, alphanum, dashes ok, underscores not
     return render_template("index.html")
 
+@app.route('/steps')
+def steps():
+    return render_template("steps.html")
+
+@app.route('/create_authzero_application')
+def create_applications():
+    session['authzero_token'] = request.form['authzero_token']
+    session['twilio_account_sid'] = request.form['twilio_account_sid']
+    session['twilio_auth_token'] = request.form['twilio_auth_token']
+    try:
+        authzero_result = create_authzero_application(session['authzero_token'])
+        if authzero_result['success'] == True:
+            print("Auth0 worked")
+    except:
+        flash("Couldn't create your Auth0 service. Double check your token?")
+        return redirect(url_for("index"))
+    try:
+        twilio_result = create_twilio_application(session['twilio_account_sid'],session['twilio_auth_token'])
+        if twilio_result['success'] == True:
+            print("Twilio worked!")
+    except:
+        print("Something didn't work with Twilio")
+    return
 
 @app.route('/check_heroku_name', methods=['POST'])
 def check_heroku_name():
-    desired_url = "https://api.heroku.com/apps/{}".format(request.form[
+    session['app_name'] = request.form['app_name']
+    desired_url = "https://api.heroku.com/apps/{}".format(session[
                                                           'app_name'])
     headers = {'Content-type': 'application/json',
                'Accept': 'application/vnd.heroku+json; version=3'}
@@ -30,7 +68,7 @@ def check_heroku_name():
 
     if session['name_available']:
         flash('Your Heroku Name is available!')
-        return redirect(url_for('create_heroku_account'))
+        return redirect(url_for('steps'))
 
     else:
         app_name_already_taken_message = 'Sorry, the name {} is already taken'.format(
@@ -39,43 +77,9 @@ def check_heroku_name():
         return redirect(url_for('index'))
 
 
-@app.route('/create_heroku_account')
-def create_heroku_account():
-    #Get the user to sign up for Heroku - No API for this
-    return render_template("heroku.html")
 
 
-@app.route('/create_authzero_account')
-def create_authzero_account():
-    #Get the user to sign up for Auth0 - No API for this
-    #Walk the user through creating an API key / token
-    return()
 
-
-@app.route('/create_authzero_application')
-def create_authzero_application():
-    #Create a new client application
-        #Params are Callback, Logout, CORS, Web Origins, OIDC Off
-    #Save Domain Name, Client ID, Client Secret from response
-    #Create Auth0 rule
-    return()
-
-
-@app.route('/create_twilio_account')
-def create_twilio_account():
-    #Get the user to sign up for Twilio - No API for this
-    return()
-
-
-@app.route('/create_twilio_application')
-def create_twilio_application():
-    #Collect the User's account Sid, Message Service Sid, Auth Token
-    #Create a Twilio Messaging Service https://www.twilio.com/docs/sms/services/api#create-a-service
-        # Params: Process Inbound Messages, Inbound Request URL, Status Callback URL
-    # Collect account_sid, sid
-    # From somewhere else? collect Twilio auth token
-    # Purchase a Twilio number and tell the user what it is
-    return()
 
 
 @app.route('/deploy')
